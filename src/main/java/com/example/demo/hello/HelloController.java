ee7290bb37cd3fb65207667b4d88cca7825f7368
Shogoohashi <syougo.work9139@gmail.com>
2022-03-20 10:22:33 +0900

    第３章　Hello World

---
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController{
+	
+	@GetMapping("/hello")
+	public String getHello() {
+		return "hello";
+		
+	}
+	
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1")String str,Model model) {
+		
+		model.addAttribute("sample",str);
+		
+		return "hello/response";
+	}
+}
